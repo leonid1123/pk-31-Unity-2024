f79db973c52c503a18d3c6542ed04699615b530e
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2024-09-27 09:01:02 +0300

    apple

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     float mov;
     [SerializeField]
     float spd;
+    bool toRight = true;
     void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
@@ -19,5 +20,15 @@ public class PlayerController : MonoBehaviour
         mov = Input.GetAxis("Horizontal");
         rb2d.velocity = new Vector3(mov*spd*Time.deltaTime,rb2d.velocityY,0);
         anim.SetFloat("movement",Mathf.Abs(mov));
+
+        if (rb2d.velocityX>0 & !toRight)
+        {
+            gameObject.transform.Rotate(0, 180, 0);
+            toRight = true;
+        } else if(rb2d.velocityX < 0 & toRight)
+        {
+            gameObject.transform.Rotate(0, 180, 0);
+            toRight = false;
+        }
     }
 }
