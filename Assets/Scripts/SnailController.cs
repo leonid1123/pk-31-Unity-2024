6223ac113b6a7c3c61c38c8045cb32f553c1de35
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2024-11-11 22:20:35 +0300

    snail movement

--- Assets/Scripts/SnailController.cs
+++ Assets/Scripts/SnailController.cs
@@ -1,25 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SnailController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other) 
-    {
-        if (other.name =="Player") {
-            Destroy(this.gameObject);
-        }
-        
-    }
-    // Start is called before the first frame update
+    public Transform boundaryLeft; // Левый предел
+    public Transform boundaryRight; // Правый предел
+    public float speed = 2f; // Скорость движения
+    private bool movingRight = false; // Направление движения
+    private Rigidbody2D rb2d; // Компонент Rigidbody2D
+    
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>(); // Получаем компонент Rigidbody2D
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 movement = Vector2.zero; // Вектор движения
+
+        if (movingRight)
+        {
+            movement = Vector2.right * speed; // Движение вправо
+
+            // Проверка на достижение правой границы
+            if (transform.position.x >= boundaryRight.position.x)
+            {
+                movingRight = false; // Меняем направление
+                Flip();
+            }
+        }
+        else
+        {
+            movement = Vector2.left * speed; // Движение влево
+
+            // Проверка на достижение левой границы
+            if (transform.position.x <= boundaryLeft.position.x)
+            {
+                movingRight = true; // Меняем направление
+                Flip();
+            }
+        }
+
+        // Применяем движение к Rigidbody2D
+        rb2d.velocity = new Vector2(movement.x, rb2d.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Обработка столкновений, если необходимо
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Логика столкновения с игроком
+            Debug.Log("Враг столкнулся с игроком!");
+        }
+    }
+
+    private void Flip()
+    {
         
+        transform.Rotate(0, 180, 0);
     }
 }
